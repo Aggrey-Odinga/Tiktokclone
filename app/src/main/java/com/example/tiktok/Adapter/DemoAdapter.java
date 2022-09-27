cf4aa53c0c6d2a3b06171d8ab0fd9443df9835d9
Aggrey-Odinga <aggreyodinga@gmail.com>
2022-09-27 09:32:03 +0300

    Add data and layout view to recyclerview

--- app/src/main/java/com/example/tiktok/Adapter/DemoAdapter.java
+++ app/src/main/java/com/example/tiktok/Adapter/DemoAdapter.java
@@ -1,5 +1,60 @@
 package com.example.tiktok.Adapter;
 
-public class DemoAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.tiktok.Models.MediaObject;
+import com.example.tiktok.R;
+
+import java.util.List;
+
+public class DemoAdapter extends RecyclerView.Adapter<DemoAdapter.DemoViewHolder> {
+
+    List<MediaObject> mediaObjectList;
+    Context context;
+
+    public DemoAdapter(List<MediaObject> mediaObjectList, Context context) {
+        this.mediaObjectList = mediaObjectList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public DemoViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+      View view = LayoutInflater.from(viewGroup.getContext()).inflate( R.layout.layout_main, viewGroup,false);
+      return new DemoViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull DemoViewHolder holder, int position) {
+
+        MediaObject mediaObject = mediaObjectList.get(position);
+
+        Glide.with(context).load(R.drawable.cd).into(holder.sound_dis);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mediaObjectList.size();
+    }
+
+    public class DemoViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView sound_dis;
+
+        public DemoViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            sound_dis = (ImageView) itemView.findViewById(R.id.imageView);
+        }
+    }
 }
--- app/src/main/java/com/example/tiktok/HomeActivity.java
+++ app/src/main/java/com/example/tiktok/HomeActivity.java
@@ -1,23 +1,65 @@
 package com.example.tiktok;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.example.tiktok.Adapter.DemoAdapter;
+import com.example.tiktok.Models.MediaObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     private ImageView sound_dis;
-
+    private List<MediaObject> mediaObjectList = new ArrayList<>();
+    private DemoAdapter demoAdapter;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        sound_dis = (ImageView) findViewById(R.id.imageView);
-        Glide.with(this).load(R.drawable.cd).into(sound_dis);
+        init();
     }
+    private void init(){
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        SnapHelper mSnapHelper = new PagerSnapHelper();
+        mSnapHelper.attachToRecyclerView(recyclerView);
+
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+        mediaObjectList.add(new MediaObject("","", "", "", "", "", "", "", "",""));
+
+        demoAdapter = new DemoAdapter(mediaObjectList, getApplicationContext());
+        recyclerView.setAdapter(demoAdapter);
+        demoAdapter.notifyDataSetChanged();
+
+
+    }
+
 }
